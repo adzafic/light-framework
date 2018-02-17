98576fe4d388210d88ea62eb17adc0fae74b02cc
adzafic <adnan.dzafic21@gmail.com>
2018-02-17 19:16:50 +0100

    añadir conexion a base de datos y ejemplo de modelo

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+    //db params
+    define('DB_HOST','172.17.0.3');
+    define('DB_USER','root');
+    define('DB_PASS','123456');
+    define('DB_NAME','test_framework');
+
     //app root
     define('APPROOT',dirname(dirname(__FILE__)));
     //URL root
